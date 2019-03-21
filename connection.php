a8a2ecfae77b3deffa8b8b5ac3b41f77f6f6a2a0
Luthfi Alfarisi <luthfialfarizi98@gmail.com>
2019-03-21 19:27:42 +0700

    Update 19 Mar

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-	// PHP Data Objects(PDO) Sample Code:
+	/*
 	try {
 		$conn = new PDO("sqlsrv:server = tcp:luthfiwebserver.database.windows.net,1433; Database = luthfi_db", "luthfi", "vhs4bdgGOOD");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -9,9 +9,19 @@
 		die(print_r($e));
 	}
 
-	// SQL Server Extension Sample Code:
 	$connectionInfo = array("UID" => "luthfi@luthfiwebserver", "pwd" => "vhs4bdgGOOD", "Database" => "luthfi_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 	$serverName = "tcp:luthfiwebserver.database.windows.net,1433";
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	*/
 	
+	$host = "luthfiwebserver.database.windows.net";
+    $user = "luthfi";
+    $pass = "vhs4bdgGOOD";
+    $db = "luthfi_db";
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 ?>
